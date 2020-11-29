75f062f1564b898b9406bdf9e2e95191dddb7ba5
Prakarsh Gupta <pg12@linux-a2.ews.illinois.edu>
2020-11-29 01:34:49 -0600

    GraphADT implemented

--- headers/Graph.h
+++ headers/Graph.h
@@ -6,26 +6,38 @@
 #include "Edge.h"
 
 // // // Modules // // //
-#include <iostream>
 #include <unordered_map>
-#include <list> 
+#include <list>
+#include <vector> 
 
+using std::unordered_map;
+using std::list;
+using std::vector;
 
 class Graph{
   public:
     Graph();
-    void insertVertex(Vertex& v);
-    void removeVertex(Vertex& v);
-    bool areAdjacent(Vertex& v1, Vertex& v2);
-    void insertEdge(Vertex& v1, Vertex& v2); // Use Disjoint Sets + Path Compression to check for good query
-    void removeEdge(Vertex& v1, Vertex& v2);
-    std::list<EdgeListIterator>& incidentEdges(Vertex& v);
+    void insertVertex(const Vertex& v);
+    void removeVertex(const Vertex& v);
+    bool edgeExists(const Vertex& source,const Vertex& destination);
+    bool vertexExists(const Vertex& v);
+    void insertEdge(Vertex& source,Vertex& destination); // Use Disjoint Sets + Path Compression to check for good query
+    void removeEdge(const Vertex& source,const Vertex& destination);
+    vector<Vertex> incidentVertices(const Vertex& v);
+    vector<Edge> incidentEdges(const Vertex& v);
     void displayGraph();
   private:
-    //
-    std::unordered_map<size_t, Vertex> vertexMap_;
-    std::unordered_map<size_t, std::list<EdgeListIterator>> adjList_;
-    std::list<Edge> edgeList_;
-    size_t edgeNum_;
-  };
+    unordered_map<Vertex, unordered_map<Vertex, Edge, VertexHashFunction>, VertexHashFunction> adjacency_list;
+};
+
+
+/*
+  A=>B=>A-B  
+     C=>A-C
+     D=>D-A // store vertices which have dest to A in a vector
+  B => 
+  C =>
+  D => A => D-A
+vec[i][A]
+*/
 
