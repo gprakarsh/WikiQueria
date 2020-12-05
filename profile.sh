7b2289eb52065d989bfc78c42fe322e21dcf8ae9
Sahan Kumarasinghe <sahansk2@illinois.edu>
2020-12-05 13:00:12 -0500

    Updated profile.sh to handle arguments for sampling

--- profile.sh
+++ profile.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+NUM_SAMPLES=$1
+NUM_START=$2
+NUM_END=$3
 
-for thousands in {10..30}
+if [ -z $3 ]
+then
+echo "Usage: ./profile.sh 2>&1 NUM_SAMPLES NUM_START NUM_END"
+echo "NUM_START and NUM_END are in samples"
+exit 1
+fi
+
+for ((i=NUM_START;i<=NUM_END;i++));
 do
-RUN=$(( thousands * 10000 ))
-for sample in {1..5}
+RUN=$(( i * 10000 ))
+for ((sample=1;sample<=NUM_SAMPLES;sample++));
 do
 	/usr/bin/time --format="$RUN,$sample,%M" ./finalproj>/dev/null ../large-names.csv ../large-edge.txt $RUN
 if [ $? -ne 0 ]
