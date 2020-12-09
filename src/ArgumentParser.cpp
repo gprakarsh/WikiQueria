62f72e9131b12ca579443f397075608c8cb20439
Sahan Kumarasinghe <sahansk2@illinois.edu>
2020-12-09 12:35:19 -0500

    Revert "Updated finalproj binary argument parsing"
    This reverts commit 951256384ba1c08aa60cb8bccbd95a2b6cc4c7c8.

--- src/ArgumentParser.cpp
+++ src/ArgumentParser.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <unordered_map>
 #include "ArgumentParser.h"
-
 ArgumentParser::ArgumentParser(std::string program_name) {
     /* nothing */
     program_name_ = program_name;
